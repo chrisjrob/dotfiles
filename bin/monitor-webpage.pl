fdeb42fad1194ae467f4e3181ff102e154d706b2
chrisjrob <chrisjrob@gmail.com>
2021-05-07 11:31:39 +0100

    only compare text not full html

--- bin/monitor-webpage.pl
+++ bin/monitor-webpage.pl
@@ -7,8 +7,9 @@
 
 my $webpage = $ARGV[0];
 my $file    = $ARGV[1];
+my $count   = @ARGV;
 
-if (${ @ARGV } < 2) {
+if ($count < 2) {
     print "monitor-webpage https://example.com/page /home/user/page\n";
 }
 
@@ -18,10 +19,35 @@ if (-e $file) {
     print "Hmm $file does not exist\n";
 }
 
+dump_text($webpage, $file);
 
-system('/usr/bin/wget', '-O', $file, $webpage);
+compare_pages("$file.old", $file);
 
-if ( (-e $file) and (-e "$file.old") ) {
-    system('/usr/bin/diff', "$file.old", $file);
+exit;
+
+sub dump_page {
+    my ($webpage, $file) =@_;
+
+    system('/usr/bin/wget', '--quiet', '-O', $file, $webpage);
+
 }
 
+sub dump_text {
+    my ($webpage, $file) =@_;
+
+    my $content = `lynx --dump $webpage`;
+    
+    open(my $fh , ">", $file) or die "Cannot write to $file: $!";
+    print $fh $content;
+    close($fh) or die "Cannot close $file: $!";
+
+}
+
+sub compare_pages {
+    my ($old, $new) = @_;
+
+    if ( (-e $old) and (-e $new) ) {
+        system('/usr/bin/diff', $old, $new);
+    }
+
+}
